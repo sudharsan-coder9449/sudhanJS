d6db5822cb0e538a8a3cbf46cfade11dd8bd6ee5
sudharsan-coder9449 <ssudharsan097@gmail.com>
2025-11-06 21:37:21 +0530

    Add files via upload

---
+++ nonprimitive.js
@@ -0,0 +1,39 @@
+Array,Object,Function
+
+ Array
+
+var arr = [1,2,3];
+
+console.log(arr);
+// Object
+
+let obj = {name:"HTML",Subject:{MERN:"Arun Sir",Classroom:12},fees:450000}
+
+
+//console.log(obj.Subject.Classroom);
+
+let arr = [{name:"Css",Subject:{MERN:"Arun Sir",Classroom:12},fees:450000},{name:"HTML",Subject:{MERN:"Arun Sir",Classroom:12},fees:450000}]
+
+
+// console.log(arr[1].name);
+
+
+// const dats = arr.map((e)=>e.name)
+
+// console.log(dats);
+
+
+
+const add_1 = () => console.log('This is function');
+    
+
+
+// const add = (gjjfgjk,one)=>{
+
+//     console.log(gjjfgjk,one);
+    
+
+// }
+
+// add("This 9")
+
