b704ea14af966a43d8ade822deea56b5266d18de
sudharsan-coder9449 <ssudharsan097@gmail.com>
2025-11-06 21:38:54 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,144 @@
+//const arr = [1,2,3,4,5,6];
+
+//console.log('original array',arr);
+
+
+//arr.push(4,6,8)
+
+//console.log('after push',arr);
+
+
+// pop
+
+//const arr = [1,2,3,4,5,6,7,8,9];
+//console.log('original array',arr);
+//arr.pop()
+
+//console.log('after pop',arr);
+
+
+// shift
+
+//const arr = [1,2,3,4,5,6,7,8];
+
+//arr.shift()
+//console.log(arr);
+
+// unshift
+
+//const arr = [1,2,3,4,5,6,7,8,9];
+//arr.unshift(1,2,3,4,5,6)
+
+//console.log(arr);
+
+
+//const arr = [1,2,3,4,5,6,7,8,9];
+
+//const arr1 = [1,23,45,67,];
+
+//const arr2 = [12,34,67,89,10];
+
+//let c = arr.concat(arr1,arr2)
+
+//console.log(c);
+
+
+//join()
+
+//const arr = [1,2,3,4,5,6,,7,8,9];
+
+//const newarr = arr.join("---")
+
+//console.log(newarr);
+
+//reverse
+
+//let a = [1,2,3,4,5,6,7];
+//a.reverse()
+
+//console.log(a);
+
+// Array.slice(start,end)
+
+
+//const a = ["welcome","to","Ascard"];
+
+//console.log(a);
+
+//const part = a.slice(0,3)
+
+//console.log(a);
+
+//console.log(part);
+
+
+//splice
+
+//Array.splice(start,deletecount,newarray)
+
+//const arr = ["Thor","Supereman","Ironman","Captain America","Hulk"];
+
+//arr.splice(2,1)
+
+//console.log(arr);
+
+//include()
+
+//const arr = [1,2,3,4,5,6,7,8];
+
+//const value = arr.includes(4);
+
+//console.log(value);
+
+
+//indexof 
+
+//const arr = [1,2,3,4,5,6,7,8];
+
+//const index = arr.indexOf(8);
+
+//console.log(index);
+
+
+
+//syntax
+
+//const arr = [1,2,3,4,5,6,7,8];
+//arr.forEach((currentvalue,index)=>{
+
+ //console.log(currentvalue*2);
+    
+
+
+//})
+
+
+
+//map
+
+//const arr = [1,2,3,45,89,];
+
+//arr.map((e,i,)=>{
+
+ //console.log(e,i);
+    
+
+//})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
